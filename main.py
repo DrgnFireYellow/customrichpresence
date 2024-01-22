2cbcf0cae494e0171311ce5bb4a6531a03fe0884
DrgnFireYellow <DrgnFireYellow@users.noreply.github.com>
2024-01-22 17:06:06 -0500

    Fix the difference between details and state

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from time import sleep
 client_id = questionary.text("Enter the client id for your discord application:").ask()
 RPC = Presence(client_id)
 RPC.connect()
-state = questionary.text("Enter the first line of your custom rich presence:").ask()
-details = questionary.text("Enter the rest of your custom rich presence:").ask()
+details = questionary.text("Enter all but the last line of your custom rich presence:").ask()
+state = questionary.text("Enter the last line of your custom rich presence:").ask()
 large_image = questionary.text("Enter your image key:").ask()
 RPC.update(state=state, details=details, large_image=large_image)
 while True:
